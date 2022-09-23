1211fb74a24a888d95a8d05daebefeedd2b51f19
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-09-23 04:42:10 +0100

    Adding programs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 a = len(sys.argv) - 1
-print("{} {}:".format(a, "argument" if a == 1 else "arguments"))
+print("{} {}".format(a, "arguments." if a == 0 elif a == 1 "argument:" else "arguments:"))
 for i in range(1, a):
 	print("{:d}: {}".format(i, sys.argv[i]))
 if __name__ == "__main__":

21f897f3dcaaa2f9cba8d2fa3ed6f048f8754612
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-09-23 05:28:37 +0100

    Adding programs

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,31 +4,31 @@ from calculator_1 import add, sub, mul, div
 operator = ['+', '-', '*', '/']
 if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py {} operator {}".format('<a>', '<b>'))
-    if __name__ == "__main__":
-        pass
-    sys.exit(1)
+    flag = 1
 else:
     a = int(sys.argv[1])
     j = sys.argv[2]
     b = int(sys.argv[3])
     for x in operator:
-        if j == x:
-            break
-        elif j != x and x != '/':
-            continue
-        elif j != x and x == '/': 
-            print("Unknown operator. Available operators: +, -, *, and /")
-            if __name__ == "__main__":
-                pass
-            sys.exit(1)
-    if j == '+':
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif j == '-':
-        print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
-    elif j == '*':
-        print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
-    else:
-        print("{:d} + {:d} = {:d}".format(a, b, int(div(a, b))))
-    if __name__ == "__main__":
-        pass
+        if j != x:
+            if x != '/':
+                continue
+            else:
+                print("Unknown operator. Available operators: +, -, *, and /")
+                flag = 1
+        else:
+            if j == '+':
+                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+            elif j == '-':
+                print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
+            elif j == '*':
+                print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
+            else:
+                print("{:d} + {:d} = {:d}".format(a, b, int(div(a, b))))
+            flag = 0
+if __name__ == "__main__":
+    pass
+if flag == 0:
     sys.exit(0)
+else:
+    sys.exit(1)

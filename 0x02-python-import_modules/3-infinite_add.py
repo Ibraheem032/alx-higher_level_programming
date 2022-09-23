a7110893b39d6c7cd4cb508e3cf4e96f212d0e86
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-09-23 04:57:41 +0100

    Adding programs

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 import sys
 a = len(sys.argv) - 1
 result = 0
-for i in range(1, a):
+for i in range(1, a  + 1):
     result = result + int(sys.argv[i])
 print("{}".format(str(result)))
 if __name__ == "__main__":

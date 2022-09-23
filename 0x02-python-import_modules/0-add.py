3fe0c42ac679a479b0ae539c49d86d8ff25a89c3
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-09-23 04:35:40 +0100

    Adding programs

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as mix
+from add_0 import add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, mix.add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 if __name__ == "__main__":
     pass

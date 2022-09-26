6f44a1e17dcee301cc43fada1ce2dbd929a95e41
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-09-26 05:16:10 +0100

    Adding programs

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len1 = len(tuple_a)
-    len2 = len(tuple_b)
-    res = []
-    if len1 >= len2:
-        a = len1
-        if len2 == 0:
-            tuple_b = (0, 0)
-        else:
-            tuple_b = (tuple_b[0], 0)
+    a = list(tuple_a)
+    b = list(tuple_b)
+    ret = []
+    len1 = len(a)
+    len2 = len(b)
+    if len1 > len2:
+        dif = len1 - len2
+        for i in range(0, dif):
+            b.append(0)
+        print(a,b)
+        for j in range(0, len1):
+            ret.append(a[j] + b[j])
+    elif len2 > len1:
+        dif = len2 - len1
+        for i in range(0 ,dif):
+            a.append(0)
+        print(a,b)
+        for j in range(0, len2):
+            print(a[j], b[j])
+            ret.append(a[j] + b[j])
     else:
-        a = len2
-        if len1 == 0:
-            tuple_a = (0, 0)
-        else:
-            tuple_a = (tuple_a[0], 0)
-    for i in range(0, a):
-        res.append(tuple_a[i] + tuple_b[i])
-    return (tuple(res))
+        for i in range(0, len(tuple_a)):
+            ret.append(tuple_a[i] + tuple_b[i])
+    return (tuple(ret))

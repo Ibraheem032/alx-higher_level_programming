f33297f869ba3c722e84463fc176f6cf911513e3
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-10-02 01:50:06 +0100

    Updating files

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary.keys():
-		# for i in a_dictionary:
-		#if i == key:
         new_dict = a_dictionary.pop(key)
         return (new_dict)
     else:

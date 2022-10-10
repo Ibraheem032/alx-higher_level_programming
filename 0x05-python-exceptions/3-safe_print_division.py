51f54fe332b810ca2433337e212f093b5329339f
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-10-11 00:14:29 +0100

    Adding files

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        res = a / b
+    except (ValueError, TypeError, ArithmeticError):
+        pass
+        f_res = "None"
+        return (None)
+    else:
+        f_res = str(res)
+        return (res)
+    finally:
+        print("Inside result: {}".format(f_res))

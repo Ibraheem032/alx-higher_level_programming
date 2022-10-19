acd587996f5631f24c20d531783381090349d30a
Ibraheem032 <iabdulwakeel032@stu.ui.edu.ng>
2022-10-19 08:39:36 +0100

    Updating 0-add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,8 +10,34 @@ The module supplies one function, add_integer(). For example;
 
 def add_integer(a, b=98):
     """Addition of two integers
-    If a or b is a float, it is casted into an integer.
-    """
+     If a or b is a float, it is casted into an integer.
+    >>> add_integer(1, 6)
+    7
+    >>> add_integer(7.3, 6)
+    13
+    >>> add_integer(7.3, 8.8)
+    15
+    >>> add_integer(7.3, "school")
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "<stdin>", line 9, in add_integer
+    TypeError: b must be an integer
+    >>> add_integer("school", 4)
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "<stdin>", line 5, in add_integer
+    TypeError: a must be an integer
+    >>> add_integer(None)
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "<stdin>", line 5, in add_integer
+    TypeError: a must be an integer
+    >>> add_integer(7.3, -8.8)
+    -1
+    >>> add_integer(-7.3, -8.8)
+    -15  
+
+	"""
     if type(a) == int or type(a) == float:
         x = int(a)
     else:
